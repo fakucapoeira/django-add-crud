d1f30e5d855c56bcd747967baf328f0186749d2a
Facundo Garcia Cabot <faku.capoeira@gmail.com>
2024-04-25 08:35:12 -0300

    secret key change

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i7u&3+uu(dnctu^(umyiqhwp&t$pr0rb-h5c-so*&!n*hevlm^'
+#SECRET_KEY = 'django-insecure-i7u&3+uu(dnctu^(umyiqhwp&t$pr0rb-h5c-so*&!n*hevlm^'
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
